abb568bd4429bda5b8acc6e7909a405cd04378bf
NJ <nj@NJs-MacBook-Pro.lan>
2024-06-22 09:46:47 -0700

    Turn off pm2 watch

--- bin/ecosystem.config.js
+++ bin/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name   : "EZE-PAYMENTS",
     script : "./dist/index.js",
-    watch: true, // any changes to app folder will get pm2 to restart app
+    watch: false, // any changes to app folder will get pm2 to restart app
     exec_mode: 'cluster', // Set the execution mode to 'cluster'
   }]
 }
